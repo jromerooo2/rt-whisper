c17fdf40c8560c1cc981e941045ea3b5843ee27b
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2023-01-03 20:00:52 -0600

    add: record messages to transcribe them

---
+++ src/static/index.js
@@ -0,0 +1,20 @@
+        // /* global Peer */
+
+        // /**
+        //  * Gets the local audio stream of the current caller
+        //  * @param callbacks - an object to set the success/error behavior
+        //  * @returns {void}
+        //  */
+
+        //  function getLocalStream() {
+        //     navigator.mediaDevices.getUserMedia({video: false, audio: true}).then((stream) => {
+        //         window.getLocalStream = stream;
+        //         window.localAudio.srcObject = stream;
+        //         window.localAudio.autoplay = true;
+        //     }).catch((err) => {
+        //         console.error(`you got an error: ${err}`)
+        //     });
+        // }
+
+        // console.log('Hello from the browser console!');
+        // getLocalStream();
